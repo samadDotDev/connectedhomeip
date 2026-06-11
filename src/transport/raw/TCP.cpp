a62dde8ac8488f7fd53ff081500924891597395e
Abdul Samad <abdul.samad@smartthings.com>
2026-06-11 10:33:55 -0500

    more TCP logging

--- src/transport/raw/TCP.cpp
+++ src/transport/raw/TCP.cpp
@@ -179,8 +179,6 @@ ActiveTCPConnectionState * TCPBase::AllocateConnection(const Inet::TCPEndPointHa
 
             if (!activeConnection->InUse() && (activeConnection->GetReferenceCount() != 0))
             {
-                char addrStr[Transport::PeerAddress::kMaxToStringSize];
-                activeConnection->mPeerAddr.ToString(addrStr);
                 ChipLogError(Inet, "Leaked TCP connection %p to %s.", activeConnection, addrStr);
                 // Try to notify callbacks in the hope that they release; the connection is no good
                 CloseConnectionInternal(*activeConnection, CHIP_ERROR_CONNECTION_CLOSED_UNEXPECTEDLY, SuppressCallback::No);
